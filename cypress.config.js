4c87ccb6688f5a8cc5bda2c5c81577787634eb0c
Renan Marques <renanmarques33@gmail.com>
2025-11-03 12:05:39 -0300

    ChangesOnConfig

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
+
+  video: true,
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
   e2e: {
     
     chromeWebSecurity: false,
